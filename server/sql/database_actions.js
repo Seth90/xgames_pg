1863958400afd8bbeb791a9fae1f937578dc5304
AndreiG <seth_90@mail.ru>
2023-05-11 10:37:32 +0300

    Update database_actions.js

--- server/sql/database_actions.js
+++ server/sql/database_actions.js
@@ -1,12 +1,1 @@
-const { Pool } = require('pg');
- 
-module.exports = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'xgames',
-    password: 'goaldemo',
-    port: 5432,
-    max: 20,
-    idleTimeoutMillis: 60000,
-    connectionTimeoutMillis: 20000,
-});
+
